97cd50edb5ab02436b0cd0ba78595fbf7ec6416a
Mohamamad Jrab <mo.jrab02@gmail.com>
2026-02-26 09:35:00 +0300

    FIx Carousel in news details.

--- app/[locale]/news/[id]/_components/carousel.tsx
+++ app/[locale]/news/[id]/_components/carousel.tsx
@@ -9,28 +9,27 @@ import {
     Carousel as ShadCNCarousel
 } from "@/components/ui/carousel";
 import Image from "next/image";
-import {useLocale} from "next-intl";
+import { useLocale } from "next-intl";
 
-export default function Carousel({imgUrls}: { imgUrls: string[] })
-{
+export default function Carousel({ imgUrls }: { imgUrls: string[] }) {
     const dir = useLocale() === 'ar' ? 'rtl' : 'ltr';
 
     return (
-        <ShadCNCarousel className="w-full mx-auto max-md:order-2"
-                        opts={{align: "start", loop: true, direction: dir}}
-                        plugins={[Autoplay({delay: 5000})]}>
+        <ShadCNCarousel className="w-full h-fit mx-auto max-md:order-2"
+            opts={{ align: "start", loop: true, direction: dir }}
+            plugins={[Autoplay({ delay: 5000 })]}>
             <CarouselContent>
                 {imgUrls.map((url, index) => (
                     <CarouselItem key={index}>
                         <div className={'relative h-96'}>
-                            <Image src={url} alt={'Image ' + index} className={'object-cover rounded-lg'} fill/>
+                            <Image src={url} alt={'Image ' + index} className={'object-cover rounded-lg'} fill />
                         </div>
                     </CarouselItem>
                 ))}
             </CarouselContent>
-            <CarouselPrevious className={'start-1 md:-start-4'}/>
-            <CarouselNext className={'end-1 md:-end-4'}/>
-            <CarouselDots/>
+            <CarouselPrevious className={'start-1 md:-start-4'} />
+            <CarouselNext className={'end-1 md:-end-4'} />
+            <CarouselDots />
         </ShadCNCarousel>
     );
 }
